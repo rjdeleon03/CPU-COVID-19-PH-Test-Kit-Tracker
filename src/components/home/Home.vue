997a2d40ef79b8451bc30db5541fd8a4d2089025
Angelica Salvo <angelicasalvogomez@gmail.com>
2020-03-24 11:00:56 +0800

    [FEATURE][UI] Initial table layout

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,15 +2,40 @@
   <div>
     <h1>Home</h1>
     <div align="center">
-      <table>
-        <tr>
-          <th>hello</th>
-          <th>hello</th>
-        </tr>
-        <tr>
-          <td>content</td>
-          <td>content</td>
-        </tr>
+      <table id="tbl-test-kits-id" border="1">
+        <thead>
+          <th colspan="5">COVID-19 PH TEST KITS</th>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Source</th>
+            <th>Nature of Acquisition</th>
+            <th>Units On-hand</th>
+            <th>Units Pledged</th>
+            <th>Units Used</th>
+          </tr>
+          <tr>
+            <td>UP NIH</td>
+            <td>Research</td>
+            <td align="center">3,000</td>
+            <td align="center">0</td>
+            <td align="center">0</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td></td>
+            <td align="center">Total</td>
+            <td align="center">3,000</td>
+            <td align="center">0</td>
+            <td align="center">0</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td align="center">Total (On-hand + Pledged)</td>
+            <td align="center" colspan="3">3,000</td>
+          </tr>
+        </tfoot>
       </table>
     </div>
   </div>
