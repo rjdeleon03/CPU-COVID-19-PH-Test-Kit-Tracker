845aa5df1d591c27460f1ad811c268ceff2195d7
Ricardo Pablo De Leon <rjdeleon@up.edu.ph>
2020-03-26 23:11:42 +0800

    [FEATURE] cleanup code

--- src/components/new-entry/NewEntry.vue
+++ src/components/new-entry/NewEntry.vue
@@ -3,35 +3,26 @@
     <h1>Create New Entry</h1>
     <div>
       <input type="text" v-model="newReptile" @keyup.enter="addReptile" />
-      <button @click="addReptile">
-        Add Reptile
-      </button>
+      <button @click="addReptile">Add Reptile</button>
     </div>
   </div>
 </template>
 
 <script>
-import { db } from "@/firebase/init";
+// import { db } from "@/firebase/init";
+// import test from "./test.json";
 export default {
   name: "NewEntry",
   components: {},
   data() {
     return {
+      seed: JSON.parse(),
       newReptile: ""
-    };
-  },
-  dao() {
-    return {
-      reptiles: db.collection("kits")
     };
   },
   methods: {
     addReptile() {
       console.log("Hello!");
-      db.collection("kits").add({
-        name: this.newReptile,
-        timestamp: new Date()
-      });
     }
   }
 };
