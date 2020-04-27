cfb40741e5ab62f7c76d60b6bc25ad2dc50147d2
rjdeleon <ricardopablo.deleon@gmail.com>
2020-04-27 21:44:31 +0800

    [CODE] Remove all unnecessary files

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,10 @@ module.exports = {
   ],
   configureWebpack: {
     devtool: "source-map"
+  },
+  chainWebpack: config => {
+    config.performance
+      .maxEntrypointSize(400000)
+      .maxAssetSize(400000)
   }
 }
